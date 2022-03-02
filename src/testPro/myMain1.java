5ee0ca4a1285a15f1d9f4ce8703f94d5abb06be5
ruphu <ruphu@10.0.0.172>
2022-03-02 11:30:57 -0500

    ArrayList Added

--- src/testPro/myMain1.java
+++ src/testPro/myMain1.java
@@ -1,9 +1,18 @@
 package testPro;
+import java.util.ArrayList;
 
 public class myMain1 {
 public static void main(String[] args) {
 	System.out.println("RJ");
 	System.out.println("God bless all");
 	System.out.println("test");
+	
+	    ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    System.out.println(cars);
+	
 }
 }
